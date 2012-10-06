424df969edd7a47a8638106abdd7d211c0be4536
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2012-10-06 02:58:38 -0400

    Okay, IO is working.

--- src/Storage.hs
+++ src/Storage.hs
@@ -21,18 +21,23 @@ get s = do
 
 -- the Bool represents sharing
 -- by which I mean whether it overwrote
---add :: String -> (String, String) -> Bool
+add :: String -> (String, String) -> IO Bool
 add s r = do
   db <- openDB
-  let (b, db') = partition (\(a, _) -> a == s) db
+  let (b, db') = partition (\x -> fst x == s) db
   let newdb = (s, r) : db'
   writeDB newdb
   return $ length b == 1
 
 writeDB :: DB -> IO ()
-writeDB db = writeFile dblocat $ show db
+writeDB db = do
+  handle <- openFile dblocat WriteMode
+  hPutStr handle $ show db
+  hClose handle
 
 openDB :: IO DB
 openDB = do
-  cons <- readFile dblocat
-  return $ read cons
+  handle <- openFile dblocat ReadMode
+  cont <- hGetLine handle
+  hClose handle
+  return $ read cont
